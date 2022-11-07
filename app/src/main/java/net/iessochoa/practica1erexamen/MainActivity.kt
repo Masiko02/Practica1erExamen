7cc981f53fd458cbfd9917c4e34a43d129b7441c
Tomas Ibarra <tomasiballe@gmail.com>
2022-11-07 17:23:32 +0100

    Interfaz 1 realizada

---
+++ app/src/main/java/net/iessochoa/practica1erexamen/MainActivity.kt
@@ -0,0 +1,10 @@
+package net.iessochoa.practica1erexamen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+    }
+}
